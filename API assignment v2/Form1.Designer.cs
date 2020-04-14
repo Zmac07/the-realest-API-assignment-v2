3f8c56457832e018a9ab3d923a91bbb05934e1ad
Zachary McCarthy <zmccarthy@westshore.edu>
2020-04-14 11:39:10 -0400

    Add project files.

---
+++ API assignment v2/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace API_assignment_v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txt_Description = new System.Windows.Forms.TextBox();
+            this.txt_DisplayStatus = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(382, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Upload";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(224, 83);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(100, 20);
+            this.txtTitle.TabIndex = 1;
+            // 
+            // txt_Description
+            // 
+            this.txt_Description.Location = new System.Drawing.Point(224, 148);
+            this.txt_Description.Name = "txt_Description";
+            this.txt_Description.Size = new System.Drawing.Size(100, 20);
+            this.txt_Description.TabIndex = 2;
+            // 
+            // txt_DisplayStatus
+            // 
+            this.txt_DisplayStatus.Location = new System.Drawing.Point(224, 213);
+            this.txt_DisplayStatus.Name = "txt_DisplayStatus";
+            this.txt_DisplayStatus.Size = new System.Drawing.Size(100, 20);
+            this.txt_DisplayStatus.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(224, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(221, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Description";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(208, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Public, Private, or Unlisted?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_DisplayStatus);
+            this.Controls.Add(this.txt_Description);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txt_Description;
+        private System.Windows.Forms.TextBox txt_DisplayStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ API assignment v2/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+            using System;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Upload;
+using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+namespace API_assignment_v2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                new UploadVideo().Run(txtTitle.Text, txt_Description.Text, txt_DisplayStatus.Text).Wait();
+                
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var er in ex.InnerExceptions)
+                {
+                   Console.WriteLine("Error: " + er.Message);
+                }
+            }
+        }
+
+    }
+
+    public class UploadVideo
+  {
+    [STAThread]
+
+        public async Task Run(string txtTitle, string txt_Description, string txt_DisplayStatus)
+    {
+      UserCredential credential;
+      using (var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+      {
+        credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+            GoogleClientSecrets.Load(stream).Secrets,
+            // This OAuth 2.0 access scope allows an application to upload files to the
+            // authenticated user's YouTube channel, but doesn't allow other types of access.
+            new[] { YouTubeService.Scope.YoutubeUpload },
+            "user",
+            CancellationToken.None
+        );
+      }
+
+      var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+      {
+        HttpClientInitializer = credential,
+        ApplicationName = Assembly.GetExecutingAssembly().GetName().Name
+      });
+
+      var video = new Video();
+      video.Snippet = new VideoSnippet();
+      video.Snippet.Title = txtTitle;
+      video.Snippet.Description = txt_Description;
+      video.Snippet.Tags = new string[] { "tag1", "tag2" };
+      video.Snippet.CategoryId = "22"; // See https://developers.google.com/youtube/v3/docs/videoCategories/list
+      video.Status = new VideoStatus();
+      video.Status.PrivacyStatus = txt_DisplayStatus; // or "private" or "public"
+      var filePath = @"C:\Users\zach\source\repos\API assignment v2\API assignment v2\bin\Debug\video0.mp4"; // Replace with path to actual movie file.
+
+      using (var fileStream = new FileStream(filePath, FileMode.Open))
+      {
+        var videosInsertRequest = youtubeService.Videos.Insert(video, "snippet,status", fileStream, "video/*");
+        videosInsertRequest.ProgressChanged += videosInsertRequest_ProgressChanged;
+        videosInsertRequest.ResponseReceived += videosInsertRequest_ResponseReceived;
+
+        await videosInsertRequest.UploadAsync();
+      }
+    }
+
+    void videosInsertRequest_ProgressChanged(Google.Apis.Upload.IUploadProgress progress)
+    {
+      switch (progress.Status)
+      {
+        case UploadStatus.Uploading:
+          Console.WriteLine("{0} bytes sent.", progress.BytesSent);
+          break;
+
+        case UploadStatus.Failed:
+          Console.WriteLine("An error prevented the upload from completing.\n{0}", progress.Exception);
+          break;
+      }
+    }
+
+    void videosInsertRequest_ResponseReceived(Video video)
+    {
+      Console.WriteLine("Video id '{0}' was successfully uploaded.", video.Id);
+    }
+  }
+}
+        
+    
+
+
+
